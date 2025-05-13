9739a9bdd7609db2ef20d906aca3dd9a0fc49e94
Calle <carl.jonsen@yh.nackademin.se>
2025-05-13 13:35:08 +0200

    Adding swagger

--- BookingEventGateway/Program.cs
+++ BookingEventGateway/Program.cs
@@ -1,23 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+﻿var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/swagger");
+    return Task.CompletedTask;
+});
 app.Run();
